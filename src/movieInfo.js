5e0089de1e127bccb31baf3749db4051e7e7f7e6
Allan Ribeiro <webdev.allan@gmail.com>
2021-11-06 16:28:55 -0300

    adição de gênero

--- src/movieInfo.js
+++ src/movieInfo.js
@@ -12,26 +12,41 @@ async function movieInfo(imdb) {
                 });
     }
     
-    function watchMovie(id) {
+    function movieProviders(id) {
         let name = "";
         return axios.get("https://api.themoviedb.org/3/movie/"+id+"/watch/providers?api_key="+env.API_KEY)
                 .then(response => {
                     const providers = response.data.results.BR.flatrate;
                     providers.forEach(element => {
-                        name = name+element.provider_name+"  ";
+                        name = name+element.provider_name+"   ";
                     });
                     return name;
                 }).catch((err) => {
-                    console.error("ops! ocorreu um erro watchMovie" + err);
+                    console.error("ops! ocorreu um erro movieProviders" + err);
+                });
+    }
+
+    function movieGenre(id){
+        let genre = "";
+        return axios.get("https://api.themoviedb.org/3/movie/"+id+"?api_key="+env.API_KEY+"&language=pt-BR")
+                .then(response => {
+                    const genres = response.data.genres;
+                    genres.forEach(element => {
+                        genre = genre+element.name+"   ";
+                    });
+                    return genre;
+                }).catch((err) => {
+                    console.error("ops! ocorreu um erro movieGenre" + err);
                 });
     }
 
     const data = await movieData(imdb);
-    const watch = await watchMovie(data.id);
-
+    const watch = await movieProviders(data.id);
+    const genre = await movieGenre(data.id);
+    
     const movie = {
         name: data.title,
-        subtitle: data.title+'\r\nAno: '+data.release_date.slice(0, 4)+
+        subtitle: data.title+'\r\n'+genre+'\r\nAno: '+data.release_date.slice(0, 4)+
         '\r\n'+data.overview+
         '\r\nOnde assistir: '+watch,
         media: "https://image.tmdb.org/t/p/original"+data.poster_path
